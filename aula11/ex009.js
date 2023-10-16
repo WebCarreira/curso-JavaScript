bdbaf2ab6137c08bcf2330b1b4178ca2687110a6
Diogo Carreira <dcweb19@gmail.com>
2023-10-16 12:03:35 +0100

    novos exercicis

---
+++ aula11/ex009.js
@@ -0,0 +1,14 @@
+var pais = 'Brasil'
+console.log(`Vivendo em ${pais}`)
+if (pais == 'Brasil'){
+    console.log('Você é Brasileiro')
+} else {
+    console.log('Você é Estrangeiro')
+}
+/*OU
+if (pais != 'Brasil') {
+    console.log('Você é estrangeiro')
+} else {
+    console.log('Você é brasileiro')
+}
+    */
