a6f0fdb34ec8fcd3fa8c52eae2d921f7b757c832
Diogo Carreira <dcweb19@gmail.com>
2023-10-20 08:35:30 +0100

    novos conhecimentos

---
+++ aula13/ambiente.js
@@ -0,0 +1,33 @@
+var c = 1 //define a variável contador = 1
+do{
+    console.log(`Passo ${c}`) //mostra o valor do contador 
+    c++ //adiciona 1 ao contador   
+}while (c <= 6)//enquanto c for igual ou menor que 6
+
+
+/*
+var c = 1 //define a variável contador = 1
+while (c <= 6) { //enquanto c for igual ou menor que 6
+    console.log(`Passo ${c}`) //mostra o valor do contador 
+    console.log('Tudo bem?') //mostra o texto
+    c++ //adiciona 1 ao contador
+}
+*/
+
+/*
+var c = 1 //define a variável contador = 1
+while (c <= 6) { //enquanto c for igual ou menor que 6
+    console.log(`Passo ${c}`) //mostra o valor do contador 
+    c++ //adiciona 1 ao contador
+}
+*/
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
